c9f26112e5329d02fd95865ab28f69decef1fad9
SATYAVEER <39643146+veermit@users.noreply.github.com>
2024-11-17 12:33:05 +0530

    Create custommalloc.c
    custom malloc with 2 without address return

---
+++ custommalloc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void cutommalloc(int size, void **ptr)
+{
+    void *temp = malloc (size);
+    if( !temp )
+    {
+        perror("error allocation");
+        return;
+    }
+    printf("address allocated %p\n", temp);
+    *ptr = temp;
+    return;
+}
+int main()
+{
+    int *ptr;
+    cutommalloc(5, (void *)&ptr);
+    printf("address custom allocated %p\n", ptr);
+
+    return 0;
+}
